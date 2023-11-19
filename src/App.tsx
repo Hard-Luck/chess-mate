850683c7aae5bce6473589b6c63dc608888c503d
mark <tyreemarka@gmail.com>
2023-11-19 16:37:08 +0000

    got connected to socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import Board from "./components/Board";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Practice from "./pages/Practice";
+import Home from "./pages/Home";
 function App() {
   return (
-    <section className="">
-      <Board />
-    </section>
+    <Routes>
+      <Route path="/practice" element={<Practice />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<h1>404</h1>} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Practice.tsx
@@ -0,0 +1,7 @@
+import Board from "@/components/Board";
+
+function Practice() {
+  return <Board />;
+}
+
+export default Practice;
